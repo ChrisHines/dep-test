c4b36136a5fc9d4fb438d155c809d4d076f697c1
Chris Hines <github@cs-guy.com>
2017-01-24 20:38:07 -0500

    Add code with Mercurial based dependency.

---
+++ dep-test.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"bitbucket.org/zombiezen/gopdf/pdf"
+)
+
+func main() {
+	doc := pdf.New()
+	canvas := doc.NewPage(pdf.USLetterWidth, pdf.USLetterHeight)
+	canvas.Translate(100, 100)
+
+	path := new(pdf.Path)
+	path.Move(pdf.Point{0, 0})
+	path.Line(pdf.Point{100, 0})
+	canvas.Stroke(path)
+
+	text := new(pdf.Text)
+	text.SetFont(pdf.Helvetica, 14)
+	text.Text("Hello, World!")
+	canvas.DrawText(text)
+
+	canvas.Close()
+
+	err := doc.Encode(os.Stdout)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
