ec1081fc288f28fb0306d656ee1621073ca3c778
Chris Hines <github@cs-guy.com>
2017-02-09 20:33:13 -0500

    Add Bazaar based dependency.

--- dep-test.go
+++ dep-test.go
@@ -1,32 +1,30 @@
 package main
 
 import (
-	"fmt"
-	"os"
-
-	"bitbucket.org/zombiezen/gopdf/pdf"
+	_ "bitbucket.org/zombiezen/gopdf/pdf"
+	_ "launchpad.net/gocheck"
 )
 
 func main() {
-	doc := pdf.New()
-	canvas := doc.NewPage(pdf.USLetterWidth, pdf.USLetterHeight)
-	canvas.Translate(100, 100)
+	// doc := pdf.New()
+	// canvas := doc.NewPage(pdf.USLetterWidth, pdf.USLetterHeight)
+	// canvas.Translate(100, 100)
 
-	path := new(pdf.Path)
-	path.Move(pdf.Point{0, 0})
-	path.Line(pdf.Point{100, 0})
-	canvas.Stroke(path)
+	// path := new(pdf.Path)
+	// path.Move(pdf.Point{0, 0})
+	// path.Line(pdf.Point{100, 0})
+	// canvas.Stroke(path)
 
-	text := new(pdf.Text)
-	text.SetFont(pdf.Helvetica, 14)
-	text.Text("Hello, World!")
-	canvas.DrawText(text)
+	// text := new(pdf.Text)
+	// text.SetFont(pdf.Helvetica, 14)
+	// text.Text("Hello, World!")
+	// canvas.DrawText(text)
 
-	canvas.Close()
+	// canvas.Close()
 
-	err := doc.Encode(os.Stdout)
-	if err != nil {
-		fmt.Fprintln(os.Stderr, err)
-		os.Exit(1)
-	}
+	// err := doc.Encode(os.Stdout)
+	// if err != nil {
+	// 	fmt.Fprintln(os.Stderr, err)
+	// 	os.Exit(1)
+	// }
 }
